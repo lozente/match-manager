9f4acd9f67ea3f4d644138c82ab5f578e0b56f63
jlee3120 <lozente@gmail.com>
2024-03-18 17:46:12 +0900

    Initialize Kotlin Spring project

---
+++ src/main/kotlin/skycock/matchmanager/MatchManagerApplication.kt
@@ -0,0 +1,11 @@
+package skycock.matchmanager
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MatchManagerApplication
+
+fun main(args: Array<String>) {
+	runApplication<MatchManagerApplication>(*args)
+}
---
+++ src/test/kotlin/skycock/matchmanager/MatchManagerApplicationTests.kt
@@ -0,0 +1,13 @@
+package skycock.matchmanager
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MatchManagerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
